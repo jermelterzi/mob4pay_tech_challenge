a5c325bfa8c8074f2f4b585b50d8bcb800a34b37
jermelterzi <joao.terzi@seventh.com.br>
2025-03-12 05:49:29 -0300

    M4P-0001 - feat: Implementing JSON serialization methods for the Customer domain class.

--- lib/src/domain/customers/models/customer.dart
+++ lib/src/domain/customers/models/customer.dart
@@ -7,7 +7,7 @@ class Customer {
   final String city;
   final String state;
 
-  Customer({
+  const Customer({
     required this.id,
     required this.name,
     required this.age,
@@ -16,6 +16,42 @@ class Customer {
     required this.city,
     required this.state,
   });
+
+  factory Customer.fromMap(Map<String, dynamic> map) {
+    return Customer(
+      id: map['id'] as int? ?? 0,
+      name: map['name'] as String? ?? '',
+      age: map['age'] as int? ?? 0,
+      email: map['email'] as String? ?? '',
+      address: map['address'] as String? ?? '',
+      city: map['city'] as String? ?? '',
+      state: map['state'] as String? ?? '',
+    );
+  }
+
+  factory Customer.fromRemoteMap(Map<String, dynamic> remoteMap) {
+    return Customer(
+      id: remoteMap['id'] as int? ?? 0,
+      name: remoteMap['nome'] as String? ?? '',
+      age: remoteMap['idade'] as int? ?? 0,
+      email: remoteMap['email'] as String? ?? '',
+      address: remoteMap['endereco'] as String? ?? '',
+      city: remoteMap['cidade'] as String? ?? '',
+      state: remoteMap['estado'] as String? ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'email': email,
+      'address': address,
+      'city': city,
+      'state': state,
+    };
+  }
 
   @override
   bool operator ==(Object other) {
--- test/src/domain/customers/models/customer_test.dart
+++ test/src/domain/customers/models/customer_test.dart
@@ -1,21 +1,48 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mob4pay_tech_challenge/src/domain/customers/models/customer.dart';
 
+import '../fixtures/customers_fixtures.dart';
+
 void main() {
   group('Customer -', () {
+    test(
+      'fromMap',
+      () {
+        // AÇÃO
+        final customer = Customer.fromMap(CustomersFixtures.tMap);
+
+        // VERIFICAÇÃO
+        expect(customer, equals(CustomersFixtures.tModel));
+      },
+    );
+
+    test(
+      'fromRemoteMap',
+      () {
+        // AÇÃO
+        final customer = Customer.fromRemoteMap(CustomersFixtures.tRemoteMap);
+
+        // VERIFICAÇÃO
+        expect(customer, equals(CustomersFixtures.tModel));
+      },
+    );
+
+    test(
+      'toMap',
+      () {
+        // AÇÃO
+        final map = CustomersFixtures.tModel.toMap();
+
+        // VERIFICAÇÃO
+        expect(map, equals(CustomersFixtures.tMap));
+      },
+    );
+
     test('Igualdade', () {
       // PREPARAÇÃO
-      final tModel = Customer(
-        id: 1,
-        name: 'João da Silva Barbosa',
-        age: 30,
-        email: 'jsilva@email.com',
-        address: 'Rua Serra da Japi',
-        city: 'São Paulo',
-        state: 'SP',
-      );
+      const tModel = CustomersFixtures.tModel;
 
-      final tEqualModel = Customer(
+      const tEqualModel = Customer(
         id: 1,
         name: 'João da Silva Barbosa',
         age: 30,
