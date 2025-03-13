070436f056a0f6aadf9b997ea3e062beee502a62
jermelterzi <joao.terzi@seventh.com.br>
2025-03-13 06:34:49 -0300

    M4P-0001 - build: Setting up the dependencies for the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mob4pay_tech_challenge/src/app.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:mob4pay_tech_challenge/src/config/dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await _initAppDatabase();
+  await setupDependencies();
 
   runApp(const MyApp());
-}
-
-Future<Database> _initAppDatabase() async {
-  final databasePath = await getDatabasesPath();
-  final database = openDatabase(
-    join(databasePath, 'mob4pay_database.db'),
-    onCreate: (db, _) {
-      return db.execute(
-        'CREATE TABLE costumers(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, email TEXT, address TEXT, city TEXT, state TEXT)',
-      );
-    },
-  );
-
-  return database;
 }
---
+++ lib/src/config/dependencies.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mob4pay_tech_challenge/src/data/customers/data_sources/customers_local_data_source.dart';
+import 'package:mob4pay_tech_challenge/src/data/customers/data_sources/customers_remote_data_source.dart';
+import 'package:mob4pay_tech_challenge/src/data/customers/repositories/customers_repository.dart';
+import 'package:mob4pay_tech_challenge/src/data/services/local_storage.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupDependencies() async {
+  getIt
+    ..registerSingletonAsync<Database>(_initAppDatabase)
+    ..registerLazySingleton<Dio>(() => Dio())
+    ..registerLazySingleton<LocalStorage>(
+      () => LocalStorageImpl(database: getIt()),
+    )
+    ..registerLazySingleton<CustomersLocalDataSource>(
+      () => CustomersLocalDataSourceImpl(localStorage: getIt()),
+    )
+    ..registerLazySingleton<CustomersRemoteDataSource>(
+      () => CustomersRemoteDataSource(getIt()),
+    )
+    ..registerLazySingleton<CustomersRepository>(
+      () => CustomersRepositoryImpl(
+        remoteDataSource: getIt(),
+        localDataSource: getIt(),
+      ),
+    );
+
+  await getIt.allReady();
+}
+
+Future<Database> _initAppDatabase() async {
+  final databasePath = await getDatabasesPath();
+  final database = openDatabase(
+    join(databasePath, 'mob4pay_database.db'),
+    version: 1,
+    onCreate: (db, _) {
+      return db.execute(
+        'CREATE TABLE costumers(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, email TEXT, address TEXT, city TEXT, state TEXT)',
+      );
+    },
+  );
+
+  return database;
+}
