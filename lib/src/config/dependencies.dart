62fdf7684e198ea81935a48c8b846ad9e983f90d
jermelterzi <joao.terzi@seventh.com.br>
2025-03-14 23:35:02 -0300

    M4P-0002 - feat: Implementing a viewmodel responsible for the state management of the customers list page

--- lib/src/config/dependencies.dart
+++ lib/src/config/dependencies.dart
@@ -5,6 +5,7 @@ import 'package:mob4pay_tech_challenge/src/data/customers/data_sources/customers
 import 'package:mob4pay_tech_challenge/src/data/customers/data_sources/customers_remote_data_source.dart';
 import 'package:mob4pay_tech_challenge/src/data/customers/repositories/customers_repository.dart';
 import 'package:mob4pay_tech_challenge/src/data/services/local_storage.dart';
+import 'package:mob4pay_tech_challenge/src/ui/customers/viewmodels/customers_viewmodel.dart';
 import 'package:mob4pay_tech_challenge/src/ui/splash/viewmodels/splash_viewmodel.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -33,6 +34,9 @@ Future<void> setupDependencies() async {
     ..registerLazySingleton<AppRouter>(() => AppRouter())
     ..registerFactory<SplashViewmodel>(
       () => SplashViewmodel(customersRepository: getIt()),
+    )
+    ..registerFactory<CustomersViewmodel>(
+      () => CustomersViewmodel(customersRepository: getIt()),
     );
 
   await getIt.allReady();
---
+++ lib/src/ui/customers/viewmodels/customers_viewmodel.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mob4pay_tech_challenge/src/data/customers/repositories/customers_repository.dart';
+import 'package:mob4pay_tech_challenge/src/domain/customers/models/customer.dart';
+
+class CustomersViewmodel extends ChangeNotifier {
+  final CustomersRepository _customersRepository;
+
+  CustomersViewmodel({required CustomersRepository customersRepository})
+      : _customersRepository = customersRepository;
+
+  List<Customer> customers = [];
+  bool isLoading = false;
+  bool hasError = false;
+
+  Future<void> getCustomers() async {
+    isLoading = true;
+    notifyListeners();
+
+    final getSavedCustomers = await _customersRepository.getCustomers();
+
+    return getSavedCustomers.fold(
+      (savedCustomers) {
+        customers = savedCustomers;
+        isLoading = false;
+        notifyListeners();
+
+        return;
+      },
+      (_) {
+        hasError = true;
+        isLoading = false;
+        notifyListeners();
+
+        return;
+      },
+    );
+  }
+}
